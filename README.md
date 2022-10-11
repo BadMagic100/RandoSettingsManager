9a41d8da06f27ede242f02196ec206468fd18249
Sean Dempsey <dempsey.sean@outlook.com>
2022-10-11 16:27:48 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# RandoSettingsManager
+
+A Hollow Knight mod that reimplements and extends Randomizer's settings sharing and preset management with rich support for connection mods.
---
+++ RandoSettingsManager/RandoSettingsManagerMod.cs
@@ -0,0 +1,39 @@
+﻿using Modding;
+using System;
+
+namespace RandoSettingsManager
+{
+    public class RandoSettingsManagerMod : Mod
+    {
+        private static RandoSettingsManagerMod? _instance;
+
+        internal static RandoSettingsManagerMod Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    throw new InvalidOperationException($"An instance of {nameof(RandoSettingsManagerMod)} was never constructed");
+                }
+                return _instance;
+            }
+        }
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+        public RandoSettingsManagerMod() : base()
+        {
+            _instance = this;
+        }
+
+        // if you need preloads, you will need to implement GetPreloadNames and use the other signature of Initialize.
+        public override void Initialize()
+        {
+            Log("Initializing");
+
+            // put additional initialization logic here
+
+            Log("Initialized");
+        }
+    }
+}
