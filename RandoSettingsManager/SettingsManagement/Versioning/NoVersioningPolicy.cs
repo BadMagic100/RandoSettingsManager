b3a18c3ab6b0cb04e42f81eccef3f0a7084088b6
Sean Dempsey <dempsey.sean@outlook.com>
2022-10-14 19:02:14 -0700

    Add NoVersioningPolicy

---
+++ RandoSettingsManager/SettingsManagement/Versioning/NoVersioningPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace RandoSettingsManager.SettingsManagement.Versioning
+{
+    public class NoVersioningPolicy : IVersioningPolicy<int>
+    {
+        public int Version => 0;
+
+        public bool Allow(int version) => true;
+    }
+}
