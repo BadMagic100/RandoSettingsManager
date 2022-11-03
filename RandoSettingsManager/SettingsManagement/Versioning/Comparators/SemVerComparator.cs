f034b3fdfc2d27b9074c68e77ac1df12bb9b341f
Sean Dempsey <dempsey.sean@outlook.com>
2022-11-02 19:52:36 -0700

    Add option for semver comparison to only compare a given number of places

--- RandoSettingsManager/SettingsManagement/Versioning/Comparators/SemVerComparator.cs
+++ RandoSettingsManager/SettingsManagement/Versioning/Comparators/SemVerComparator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RandoSettingsManager.SettingsManagement.Versioning.Comparators
@@ -9,14 +10,33 @@ namespace RandoSettingsManager.SettingsManagement.Versioning.Comparators
     public class SemVerComparator : Comparer<string>
     {
         readonly string? suffixSeparator;
+        readonly int places;
 
         /// <summary>
-        /// Constructs a SemVerComparer
+        /// Constructs a SemVerComparer with an optional suffix
         /// </summary>
         /// <param name="suffixSeparator">A string to use to remove suffixes, eg + for a mod versioned like X.Y.Z.W+hash</param>
+        [Obsolete]
         public SemVerComparator(string? suffixSeparator = null)
         {
             this.suffixSeparator = suffixSeparator;
+            this.places = -1;
+        }
+
+        /// <summary>
+        /// Constructs a SemVerComparator with an optional suffix and a specified number of decimal places
+        /// </summary>
+        /// <param name="suffixSeparator">A string to use to remove suffixes, eg + for a mod versioned like X.Y.Z.W+hash</param>
+        /// <param name="places">The number of decimal places to compare. Any negative number will compare all places available.</param>
+        public SemVerComparator(string? suffixSeparator = null, int places = -1)
+        {
+            this.suffixSeparator = suffixSeparator;
+
+            if (places == 0)
+            {
+                throw new ArgumentException("Expected nonzero number of places to compare", nameof(places));
+            }
+            this.places = places;
         }
 
         /// <inheritdoc/>
@@ -28,6 +48,11 @@ namespace RandoSettingsManager.SettingsManagement.Versioning.Comparators
             // if either version has "extra" versions, zip will ignore them; this allows, for example, saying that
             // you're backwards compatible until X.Y.*.* in a system where * is auto-set
             IEnumerable<(int, int)> versions = x.Split('.').Zip(y.Split('.'), (a, b) => (int.Parse(a), int.Parse(b)));
+            if (places > -1)
+            {
+                versions = versions.Take(places);
+            }
+
             foreach ((int a, int b) in versions)
             {
                 int c = a.CompareTo(b);
